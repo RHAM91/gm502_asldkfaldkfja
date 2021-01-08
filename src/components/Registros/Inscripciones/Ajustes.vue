322f22f89b879dec87820825277cf9f5c2340035
Romario Torres <romariotorres@vivaldi.net>
2021-01-08 12:25:12 -0600

    Mas secciones en ajustes

--- src/components/Registros/Inscripciones/Ajustes.vue
+++ src/components/Registros/Inscripciones/Ajustes.vue
@@ -2,19 +2,22 @@
     <b-container fluid>
         <b-row>
             <b-col sm="12" class="mt-3">
-                
+
                 <div class="seccion_ajustes">
                     <div class="seccion_ajustes_titulo">
                         <h4>
-                            Docentes
+                            Cursos
                         </h4>
-                        <b-button v-b-toggle.collapse-1 variant="outline-info" size="sm" style="height: 34px;margin-top: 5px;" @click="btn_docentes = !btn_docentes">{{btn_docentes ? 'Expandir': 'Contraer'}}</b-button>
+                        <b-button v-b-toggle.collapse-1 variant="outline-info" size="sm" style="height: 34px;margin-top: 5px;" @click="btn_cursos = !btn_cursos">{{btn_cursos ? 'Expandir': 'Contraer'}}</b-button>
                     </div>
                     <p>
-                        Registro de docentes
+                        Registro de cursos que se imparten en la academia
                     </p>
                     <b-collapse id="collapse-1" class="mt-2">
                         <b-card style="background-color: #fafafa;">
+                            <div class="btn_agregar">
+                                <b-button type="button" variant="success" size="sm">Nuevo registro</b-button>
+                            </div>
                             <table class="table table-bordered table-striped table-sm" style="font-size: 12px;">
                                 <thead>
                                     <tr>
@@ -29,7 +32,136 @@
                                 <tbody>
                                     <tr>
                                         <td>
-                                            Romario Torres
+                                            Registro 1
+                                        </td>
+                                        <td style="text-align: center;">
+                                            <b-button type="button" size="sm" variant="info" style="margin-right: 5px;" title="Editar"><i class="fas fa-edit"></i></b-button>
+                                            <b-button type="button" size="sm" variant="danger" style="margin-right: 5px;" title="Borrar"><i class="far fa-trash-alt"></i></b-button>
+                                            <b-button type="button" size="sm" variant="warning"  title="Información"><i class="fas fa-info-circle"></i></b-button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </b-card>
+                    </b-collapse>
+                </div>
+                
+                <div class="seccion_ajustes">
+                    <div class="seccion_ajustes_titulo">
+                        <h4>
+                            Docentes
+                        </h4>
+                        <b-button v-b-toggle.collapse-2 variant="outline-info" size="sm" style="height: 34px;margin-top: 5px;" @click="btn_docentes = !btn_docentes">{{btn_docentes ? 'Expandir': 'Contraer'}}</b-button>
+                    </div>
+                    <p>
+                        Registro de docentes
+                    </p>
+                    <b-collapse id="collapse-2" class="mt-2">
+                        <b-card style="background-color: #fafafa;">
+                            <div class="btn_agregar">
+                                <b-button type="button" variant="success" size="sm">Nuevo registro</b-button>
+                            </div>
+                            <table class="table table-bordered table-striped table-sm" style="font-size: 12px;">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 80%;">
+                                            Nombre
+                                        </th>
+                                        <th style="width: 20%;text-align: center;">
+                                            ...
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            Registro 1
+                                        </td>
+                                        <td style="text-align: center;">
+                                            <b-button type="button" size="sm" variant="info" style="margin-right: 5px;" title="Editar"><i class="fas fa-edit"></i></b-button>
+                                            <b-button type="button" size="sm" variant="danger" style="margin-right: 5px;" title="Borrar"><i class="far fa-trash-alt"></i></b-button>
+                                            <b-button type="button" size="sm" variant="warning"  title="Información"><i class="fas fa-info-circle"></i></b-button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </b-card>
+                    </b-collapse>
+                </div>
+
+                <div class="seccion_ajustes">
+                    <div class="seccion_ajustes_titulo">
+                        <h4>
+                            Ubicaciones
+                        </h4>
+                        <b-button v-b-toggle.collapse-3 variant="outline-info" size="sm" style="height: 34px;margin-top: 5px;" @click="btn_ubicaciones = !btn_ubicaciones">{{btn_ubicaciones ? 'Expandir': 'Contraer'}}</b-button>
+                    </div>
+                    <p>
+                        Registro de país, iglesia y región
+                    </p>
+                    <b-collapse id="collapse-3" class="mt-2">
+                        <b-card style="background-color: #fafafa;">
+                            <div class="btn_agregar">
+                                <b-button type="button" variant="success" size="sm">Nuevo registro</b-button>
+                            </div>
+                            <table class="table table-bordered table-striped table-sm" style="font-size: 12px;">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 80%;">
+                                            Nombre
+                                        </th>
+                                        <th style="width: 20%;text-align: center;">
+                                            ...
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            Registro 1
+                                        </td>
+                                        <td style="text-align: center;">
+                                            <b-button type="button" size="sm" variant="info" style="margin-right: 5px;" title="Editar"><i class="fas fa-edit"></i></b-button>
+                                            <b-button type="button" size="sm" variant="danger" style="margin-right: 5px;" title="Borrar"><i class="far fa-trash-alt"></i></b-button>
+                                            <b-button type="button" size="sm" variant="warning"  title="Información"><i class="fas fa-info-circle"></i></b-button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </b-card>
+                    </b-collapse>
+                </div>
+
+                <div class="seccion_ajustes">
+                    <div class="seccion_ajustes_titulo">
+                        <h4>
+                            Pastores
+                        </h4>
+                        <b-button v-b-toggle.collapse-4 variant="outline-info" size="sm" style="height: 34px;margin-top: 5px;" @click="btn_pastores = !btn_pastores">{{btn_pastores ? 'Expandir': 'Contraer'}}</b-button>
+                    </div>
+                    <p>
+                        Registro de país, iglesia y región
+                    </p>
+                    <b-collapse id="collapse-4" class="mt-2">
+                        <b-card style="background-color: #fafafa;">
+                            <div class="btn_agregar">
+                                <b-button type="button" variant="success" size="sm">Nuevo registro</b-button>
+                            </div>
+                            <table class="table table-bordered table-striped table-sm" style="font-size: 12px;">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 80%;">
+                                            Nombre
+                                        </th>
+                                        <th style="width: 20%;text-align: center;">
+                                            ...
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            Registro 1
                                         </td>
                                         <td style="text-align: center;">
                                             <b-button type="button" size="sm" variant="info" style="margin-right: 5px;" title="Editar"><i class="fas fa-edit"></i></b-button>
@@ -53,7 +185,10 @@ export default {
     name: "Ajustes",
     data() {
         return {
-            btn_docentes: true
+            btn_docentes: true,
+            btn_cursos: true,
+            btn_ubicaciones: true,
+            btn_pastores: true
         }
     },
     methods: {
@@ -77,4 +212,9 @@ export default {
             display: flex;
             justify-content: space-between;
         }
+
+            .btn_agregar{
+                width: 100%;
+                margin-bottom: 10px;
+            }
 </style>
