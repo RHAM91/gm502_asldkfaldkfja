46ea525ca26a58c0f16749079ef5c94ebd6933da
Romario Torres <romariotorres@vivaldi.net>
2021-01-19 22:03:06 -0600

    Botones de atajos para inscripcion

--- src/components/Registros/Inscripciones/Alumno/Registro.vue
+++ src/components/Registros/Inscripciones/Alumno/Registro.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="contenedor_modal">
-        <div class="cuerpo_modal" style="height: 680px;width: 800px">
+        <div class="cuerpo_modal" style="height: 680px;width: 850px">
             <div class="modal_banner dd">
                 <i class="fas fa-plus" style="margin-right: 5px;"></i>Registro alumno
             </div>
@@ -23,54 +23,75 @@
                             <label>Dirección</label>
                             <b-form-input type="text" v-model="direccion" size="sm" ></b-form-input>
                         </b-col>
-                        <b-col sm="12" md="6" class="mt-3">
+                        <b-col sm="12" md="5" class="mt-3">
                             <label>Pastor</label>
                             <select class="form-control form-control-sm" v-model="pastor" required>
                                 <option value="">Seleccionar</option>
                                 <option v-for="(item, index) in pastores" :key="index" :value="item.nombre">{{item.nombre}}</option>
                             </select>
                         </b-col>
-                         <b-col sm="12" md="6" class="mt-3">
+                        <b-col sm="12" md="1" class="mt-5">
+                            <b-button type="button" size="sm" variant="outline-warning" @click="abrir_modal_pastores">+</b-button>
+                        </b-col>
+                         <b-col sm="12" md="5" class="mt-3">
                             <label>Iglesia</label>
                             <select class="form-control form-control-sm" v-model="iglesia" required>
                                 <option value="">Seleccionar</option>
                                 <option v-for="(item, index) in iglesias" :key="index" :value="item.iglesia">{{item.iglesia}}</option>
                             </select>
                         </b-col>
-                        <b-col sm="12" md="4" class="mt-3">
+                        <b-col sm="12" md="1" class="mt-5">
+                            <b-button type="button" size="sm" variant="outline-warning" @click="abrir_modal_iglesias">+</b-button>
+                        </b-col>
+                        <b-col sm="12" md="3" class="mt-3">
                             <label>País</label>
                             <select class="form-control form-control-sm" @change="getDepartamentosxpais" v-model="pais" required>
                                 <option value="">Seleccionar</option>
                                 <option v-for="(item, index) in paises" :key="index" :value="item.pais">{{item.pais}}</option>
                             </select>
                         </b-col>
-                        <b-col sm="12" md="4" class="mt-3">
+                        <b-col sm="12" md="1" class="mt-5">
+                            <b-button type="button" size="sm" variant="outline-warning" @click="abrir_modal_paises">+</b-button>
+                        </b-col>
+                        <b-col sm="12" md="3" class="mt-3">
                             <label>Ciudad</label>
                             <select class="form-control form-control-sm" @change="getRegionxCiudad" v-model="ciudad" required>
                                 <option value="">Seleccionar</option>
                                 <option v-for="(item, index) in ciudades_array" :key="index" :value="item.ciudad">{{item.ciudad}}</option>
                             </select>
                         </b-col>
-                        <b-col sm="12" md="4" class="mt-3">
+                        <b-col sm="12" md="1" class="mt-5">
+                            <b-button type="button" size="sm" variant="outline-warning" @click="abrir_modal_ciudades">+</b-button>
+                        </b-col>
+                        <b-col sm="12" md="3" class="mt-3">
                             <label>Región</label>
                             <select class="form-control form-control-sm" v-model="region">
                                 <option value="">Seleccionar</option>
                                 <option v-for="(item, index) in regiones_array" :key="index" :value="item.region">{{item.region}}</option>
                             </select>
                         </b-col>
-                        <b-col sm="12" md="4" class="mt-3">
+                        <b-col sm="12" md="1" class="mt-5">
+                            <b-button type="button" size="sm" variant="outline-warning" @click="abrir_modal_regiones">+</b-button>
+                        </b-col>
+                        <b-col sm="12" md="3" class="mt-3">
                             <label>Docente</label>
                             <select class="form-control form-control-sm" v-model="docente" >
                                 <option value="">Seleccionar</option>
                                 <option v-for="(item, index) in docentes" :key="index" :value="`${item.nombre} ${item.apellidos}`">{{item.nombre}} {{item.apellidos}}</option>
                             </select>
                         </b-col>
-                        <b-col sm="12" md="4" class="mt-3">
+                        <b-col sm="12" md="1" class="mt-5">
+                            <b-button type="button" size="sm" variant="outline-warning" @click="abrir_modal_docentes">+</b-button>
+                        </b-col>
+                        <b-col sm="12" md="3" class="mt-3">
                             <label>Curso</label>
                             <select class="form-control form-control-sm" v-model="curso" required>
                                 <option value="">Seleccionar</option>
                                 <option v-for="(item, index) in cursos" :key="index" :value="item.curso">{{item.curso}}</option>
                             </select>
+                        </b-col>
+                        <b-col sm="12" md="1" class="mt-5">
+                            <b-button type="button" size="sm" variant="outline-warning" @click="abrir_modal_cursos">+</b-button>
                         </b-col>
                         <b-col sm="12" md="4" class="mt-3">
                             <label>Nivel</label>
@@ -110,19 +131,38 @@
         </div>
 
         <Pagos v-if="mod_pagos" />
-
+        <Pastores v-if="reg_pastores" />
+        <Iglesias v-if="reg_iglesias"/>
+        <Paises v-if="reg_paises" />
+        <Ciudades v-if="reg_ciudades" />
+        <Regiones v-if="reg_regiones" />
+        <Docentes v-if="reg_docentes" />
+        <Cursos v-if="reg_cursos" />
     </div>
 </template>
 
 <script>
 import { mapActions, mapState } from 'vuex'
 import Pagos from './Modal_Pagos.vue'
-
+import Pastores from '../Pastores/Registro.vue'
+import Iglesias from '../Iglesias/Registro.vue'
+import Paises from '../Ubicaciones/Paises/Registro.vue'
+import Ciudades from '../Ubicaciones/Ciudades/Registro.vue'
+import Regiones from '../Ubicaciones/Regiones/Registro.vue'
+import Docentes from '../Docentes/Registro.vue'
+import Cursos from '../Cursos/Registro.vue'
 
 export default {
     name: 'Formulario',
     components:{
-        Pagos
+        Pagos,
+        Pastores,
+        Iglesias,
+        Paises,
+        Ciudades,
+        Regiones,
+        Docentes,
+        Cursos
     },
     computed: {
         ...mapState(['pastores', 'paises','ciudades','docentes', 'cursos', 'regiones', 'iglesias'])
@@ -145,7 +185,14 @@ export default {
             monto: '',
             ciudades_array:[],
             regiones_array: [],
-            mod_pagos: false
+            mod_pagos: false,
+            reg_pastores: false,
+            reg_iglesias: false,
+            reg_paises: false,
+            reg_ciudades: false,
+            reg_regiones: false,
+            reg_docentes: false,
+            reg_cursos: false
 
         }
     },
@@ -234,6 +281,27 @@ export default {
         },
         modal_pagos(){
             this.mod_pagos = true
+        },
+        abrir_modal_pastores(){
+            this.reg_pastores = true
+        },
+        abrir_modal_iglesias(){
+            this.reg_iglesias = true
+        },
+        abrir_modal_paises(){
+            this.reg_paises = true
+        },
+        abrir_modal_ciudades(){
+            this.reg_ciudades = true
+        },
+        abrir_modal_regiones(){
+            this.reg_regiones = true
+        },
+        abrir_modal_docentes(){
+            this.reg_docentes = true
+        },
+        abrir_modal_cursos(){
+            this.reg_cursos = true
         },
         ...mapActions(['insert_data', 'wse'])
     },
